a3a259f9de60551669106a08b6e568d7e03f6231
aiakvlie <andrii.iakovliev@ed.ac.uk>
2024-07-19 20:23:04 +0100

    Refactor 1st step of scRNA-seq QC.

--- pipeline.R
+++ pipeline.R
@@ -2,11 +2,32 @@
 source("properties.R")
 source("helperfunctions.R")
 
-## download studies with scRNA-seq data (see full list in liver_studies.tsv)
+## Process Ramachandran et al (2019) (see full list in liver_studies.tsv)
+## -----------------------------------------------------------------------------
 this.study <- 1 ## Ramachandran et al (2019)
-study1 <- download.study(this.study, studies.dt, root.dir)
+study <- download.study(this.study, studies.dt, root.dir)
 
-this.study <- 2 ## Guilliams et al (2022)
-study2 <- download.study(this.study, studies.dt, root.dir)
+## extract archive
+cmd <- sprintf("tar -xvf %s -C %s", study$study.file, study$dir)
+system(cmd)
+
+## study variable should be a list with at least 2 elements:
+## $dir -> name of the directory containing extract from the GEO archive
+## $study.file.list -> content of filelist.txt which contains a list of all
+##                     files for the study (downloaded separately from GEO)
+sample.files <- list.files(study$dir, pattern="*.tsv.gz", full.names=TRUE)
+meta <- fread(study$study.file.list)
+stopifnot(all(basename(sample.files) %in% meta$Name))
+
+## create dir path for each sample
+meta <- fread(study$study.file.list)
+meta[, sample := gsub("(GSM\\d+).*", "\\1", Name)]
+meta[, sample.dir := file.path(study$dir, sample)]
+meta <- meta[!grep(".tar", sample)]
+meta <- meta[!grep("blood|mouse", Name)]
 
 ## QC step 1
+source("qc/qc1_emptydrop.R")
+
+this.study <- 2 ## Guilliams et al (2022)
+study <- download.study(this.study, studies.dt, root.dir)
--- qc/qc1_emptydrop.R
+++ qc/qc1_emptydrop.R
@@ -2,38 +2,70 @@
 ## load
 library(DropletUtils)
 
-## Set working directory
-study.dir <- file.path(root.dir, this.study.name)
-setwd("/Volumes/khamseh-lab/ava/ME_scRNA/")
-meta_data <- read.csv("me_metadata.txt")
-files<-c()
+prepare.10x.files <- function(this.meta, study) {
+    ## create sample subdirectory and symlink relevant files
+    this.sample.dir <- this.meta[, unique(sample.dir)]
+    sample.barcodes.file <- file.path(this.sample.dir, "barcodes.tsv")
+    sample.genes.file <- file.path(this.sample.dir, "genes.tsv")
+    sample.matrix.file <- file.path(this.sample.dir, "matrix.mtx")
 
-sample="MAB04-1-WT"
-Cell=NULL
-for (sample in files){
+    dir.create(this.sample.dir, showWarnings=FALSE)
+    if (!any(file.exists(this.meta[, file.path(this.sample.dir, Name)]))) {
+        file.symlink(this.meta[, file.path(study$dir, Name)],
+                     this.meta[, file.path(this.sample.dir, Name)])
+        file.rename(this.meta[grep("barcodes", Name), file.path(sample.dir, Name)],
+                    sample.barcodes.file)
+        file.rename(this.meta[grep("genes", Name), file.path(sample.dir, Name)],
+                    sample.genes.file)
+        file.rename(this.meta[grep("matrix", Name), file.path(sample.dir, Name)],
+                    sample.matrix.file)
+    }
 
-  print(sample)
-  dir.name<-paste0("./Raw_Data/",sample,"/outs/raw_feature_bc_matrix/")
-  dir.name
-  #system(paste0("gunzip ",dir.name,"/*"))
-  barcodes<-read.table(paste0(dir.name,"/barcodes.tsv"),header = F)
-  barcodes
-  sce <- read10xCounts(dir.name)
-  set.seed(100)
-  my.count<-counts(sce)
-  colnames(my.count)<-paste0(sample,":",barcodes$V1)
-  colnames(my.count)<-gsub(pattern = '-1',replacement = "x",colnames(my.count))
-  print("starting emptyDrops")
-  e.out <- emptyDrops(my.count)
-  head(e.out)
-  #https://support.bioconductor.org/p/123554/#123562
-  is.cell <- e.out$FDR <= 0.01
-  is.cell[is.na(is.cell)]<-FALSE
-  True_Cell<-colnames(my.count)[is.cell]
-  Cell<-c(Cell,True_Cell)
-  print("done")
-
+    symlinks <- list(sample.barcodes.file=sample.barcodes.file,
+                     sample.genes.file=sample.genes.file,
+                     sample.matrix.file=sample.matrix.file)
+    return(symlinks)
 }
 
-length(Cell)
-save(Cell,file="/Users/yaoyuelin/Desktop/GoF_DNMT3A/emptydrop.Rdata")
+if (FALSE) this.sample <- "GSM4041150" ## for profiling we use only 1 sample
+
+Cell <- NULL
+for (this.sample in meta[, unique(sample)]) {
+    cat("Analyzing sample", this.sample, "\n")
+    this.meta <- meta[sample==eval(this.sample)]
+
+    ## create 1 subdirectory per sample and relevant symlinks
+    simlinks <- prepare.10x.files(this.meta, study)
+
+    sample.barcodes <- fread(simlinks$sample.barcodes.file, header=FALSE)
+
+    ## read 10x experiment from sample dir
+    sce <- read10xCounts(this.meta[, unique(sample.dir)])
+
+    ## run emptyDrops
+    ## see https://support.bioconductor.org/p/123554/#123562 in case the matrix
+    ## has been already filtered. If it has, this step will be omitted!
+    set.seed(100)
+    my.count <- counts(sce)
+    colnames(my.count) <- paste0(sample, ":", sample.barcodes$V1)
+    colnames(my.count) <- gsub(pattern='-1', replacement="x",
+                               colnames(my.count))
+
+    cat("Starting emptyDrops\n")
+    e.out <- tryCatch(emptyDrops(my.count), error = function(e) {
+        warning("The matrix must have been pre-filtered already.")
+        return(NA)  # Return NA on error
+    })
+
+    if (is.na(e.out)) True_Cell <- colnames(my.count)
+    else {
+        is.cell <- e.out$FDR <= 0.01
+        is.cell[is.na(is.cell)] <- FALSE
+        True_Cell <- colnames(my.count)[is.cell]
+    }
+
+    Cell <- c(Cell, True_Cell)
+    cat("Done.\n")
+}
+
+save(Cell, file=file.path(study$dir, "emptydrop.Rdata"))
