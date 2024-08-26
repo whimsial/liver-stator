bc27bc89b5d29487016b81b8ca8eabbae2a42857
aiakvlie <andrii.iakovliev@ed.ac.uk>
2024-08-26 15:03:30 +0100

    Update NextFlow command.

--- stator.eddie.sh
+++ stator.eddie.sh
@@ -3,8 +3,11 @@
 ## Bash script to submit a Stator job from Wild west end nodes on Eddie assuming
 ## counts.csv and genes.csv are in /exports/eddie/scratch/aiakvlie/
 
+## load NextFlow
+module load igmm/bac/nextflow/24.04.2
+
 ## pull the latest version of the pipeline
 nextflow pull AJnsm/Stator -r main
 
 ## run the pipeline
-NXF_VER=23.04.4 nextflow run AJnsm/Stator -r main -profile eddie_singularity -params-file params.json
+NXF_VER=24.04.2 nextflow run AJnsm/Stator -r main -profile eddie_singularity -params-file stator.params.json
