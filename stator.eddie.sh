5886a290aae2e4bd0cebc7f459bdc7e35862b31a
aiakvlie <andrii.iakovliev@ed.ac.uk>
2024-10-01 13:58:59 +0100

    Update Stator configuration scripts.

--- stator.eddie.sh
+++ stator.eddie.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-## Bash script to submit a Stator job from Wild west end nodes on Eddie assuming
-## counts.csv and genes.csv are in /exports/eddie/scratch/aiakvlie/
+## Bash script to run Stator using nextflow
 
-## load NextFlow
-module load igmm/bac/nextflow/24.04.2
+## load modules
+module add roslin/openjdk/13.0.1
+# module add igmm/bac/nextflow/24.04.2
+roslin/nextflow/22.10.7
+module load singularity
 
-## pull the latest version of the pipeline
-nextflow pull AJnsm/Stator -r main
+## pull development branch of Stator
+nextflow pull AJnsm/Stator -r develop
+
+## create environment variables
+export NXF_SINGULARITY_CACHEDIR=/exports/igmm/eddie/khamseh-lab/aiakovliev/.singularity
+export SINGULARITY_CACHEDIR=/exports/igmm/eddie/khamseh-lab/aiakovliev/mycontainers
 
 ## run the pipeline
-NXF_VER=24.04.2 nextflow run AJnsm/Stator -r main -profile eddie_singularity -params-file stator.params.json
+NXF_VER=22.10.7 nextflow run whimsial/Stator -r develop -profile eddie_singularity -params-file stator.params.json -resume
